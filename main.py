97c2d131aa6a794a2faa971174383750fd122167
Jorge <jajgjorgeg@gmail.com>
2021-12-24 00:36:35 -0300

    el movimiento de la pelota es constante

--- main.py
+++ main.py
@@ -1,25 +1,39 @@
 import sys
 import pygame
-from pygame.constants import K_RIGHT
+from pygame.constants import K_RIGHT, KEYDOWN, K_d
 from pelota import BallCharacter
+# definimos la variable que contendra la cantidad de fps que tiene el juego
+FPS = 60
 # Inicializamos pygame
 pygame.init()
+
 # Muestro una ventana de 800x600
 size = 800, 600
 screen = pygame.display.set_mode(size)
+
 # Cambio el título de la ventana
 pygame.display.set_caption("Juego BALl")
+
 # Comenzamos el bucle del juego
 run = True
+
 # Definimos los fps
 clock = pygame.time.Clock()
 ball = BallCharacter()
+
 # definimos los colores que usaremos en la ventana
 black = (0, 0, 0)
 
+# En esta variable se guardan los estados de las teclas: True si estan presionados y False si no lo estan
+key_events = {
+    'key_d': False,
+    'key_a': False,
+    'static': False
+}
+
 
 while run:
-    clock.tick(60)
+    clock.tick(FPS)
     # Capturamos los eventos que se han producido
     for event in pygame.event.get():
         # defino la variable que tendra los eventos que captura del teclado
@@ -27,11 +41,23 @@ while run:
         # Si el evento es salir de la ventana, terminamos
         if event.type == pygame.QUIT:
             run = False
-        
 
-    # ejecuto la funcion que cambia de posicion la pelota
-    ball.move_right(screen)
+        #captamos el evento de la tecla d
+        if key[K_d]:
+            key_events['key_d'] = True
+        else:
+            key_events['static'] = True
+            key_events['key_d'] = False
 
+        #captamos el evento de la tecla a
+        if key[K_d]:
+            key_events['key_a'] = True
+        else:
+            key_events['static'] = True
+            key_events['key_a'] = False
+
+
+    ball.moves(screen, key_events)
     pygame.display.update()
     screen.fill(black)
     pygame.event.pump()
--- pelota.py
+++ pelota.py
@@ -12,17 +12,19 @@ class BallCharacter:
         self.y = self.initial_y
         self.speed = 1
 
-    # definimos la funcion que hara un movimiento recto hacia la derecha
-    def move_right(self, screen):
-        # para hacer que la pelota se mueva, le sumo 1 a la posicion variante
-        pygame.draw.circle(screen, (200, 0, 0), (self.x, self.y), 30)
-        self.x += 1
+    # definimos la funcion que hace los tipos de movimientos
+    def moves(self, screen, move_type):
+        if move_type['key_d']:
+            # para hacer que la pelota se mueva, le sumo 1 a la posicion variante
+            pygame.draw.circle(screen, (200, 0, 0), (self.x, self.y), 30)
+            self.x += 1
+
+        if move_type['key_a']:
+            # para hacer que la pelota se mueva, le sumo 1 a la posicion variante
+            pygame.draw.circle(screen, (200, 0, 0), (self.x, self.y), 30)
+            self.x += 1
+
+        if move_type['static']:
+            pygame.draw.circle(screen, (200, 0, 0), (self.x, self.y), 30)
+
     # esta funcion hara que la pelta vaya para la izquierdad de 1 en uno en el plano cartesiano
-
-    def move_left(self, screen):
-        pygame.draw.circle(screen, (200, 0, 0), (self.x, self.y), 30)
-        self.x -= 1
-    # esta funcion muestra cuando la pelota esta cuando no tiene movimiento
-
-    def static_move(self, screen):
-        pygame.draw.circle(screen, (200, 0, 0), (self.x, self.y), 30)
