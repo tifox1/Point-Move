68e64b9faa0ebd83337f46d212d96d8c14559564
Jorge <jajgjorgeg@gmail.com>
2021-12-26 17:10:49 -0300

    movimiento parabolicos mal implementados

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sys
 import pygame
-from pygame.constants import K_RIGHT, KEYDOWN, K_d
+from pygame.constants import K_RIGHT, KEYDOWN, K_a, K_d
 from pelota import BallCharacter
 # definimos la variable que contendra la cantidad de fps que tiene el juego
 FPS = 60
@@ -12,7 +12,7 @@ size = 800, 600
 screen = pygame.display.set_mode(size)
 
 # Cambio el título de la ventana
-pygame.display.set_caption("Juego BALl")
+pygame.display.set_caption("Simulacion")
 
 # Comenzamos el bucle del juego
 run = True
@@ -28,12 +28,16 @@ black = (0, 0, 0)
 key_events = {
     'key_d': False,
     'key_a': False,
+    'key_space': False,
     'static': False
 }
 
 
 while run:
     clock.tick(FPS)
+    # print("(", ball.x, "\t", ball.y, ")")
+    print(ball.ball_clock)
+
     # Capturamos los eventos que se han producido
     for event in pygame.event.get():
         # defino la variable que tendra los eventos que captura del teclado
@@ -42,24 +46,35 @@ while run:
         if event.type == pygame.QUIT:
             run = False
 
-        #captamos el evento de la tecla d
+        # captamos el evento de la tecla d
         if key[K_d]:
             key_events['key_d'] = True
         else:
             key_events['static'] = True
             key_events['key_d'] = False
 
-        #captamos el evento de la tecla a
-        if key[K_d]:
+        # captamos el evento de la tecla a
+        if key[K_a]:
             key_events['key_a'] = True
         else:
             key_events['static'] = True
             key_events['key_a'] = False
 
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                key_events['key_space'] = True
+            else:
+                key_events['static'] = True
+                key_events['key_events'] = False
 
+    # una vez obtenidos los resultados finales de key_events
+    # pasamos el estado de los teclados para hacer los moviminentos
     ball.moves(screen, key_events)
+
+    # actualiza la ventana, de lo contrario todo quedara estatico
     pygame.display.update()
     screen.fill(black)
     pygame.event.pump()
+
 # Salgo de pygame
 pygame.quit()
--- pelota.py
+++ pelota.py
@@ -1,30 +1,45 @@
 import pygame
-
+import numpy as np
+import math 
 
 class BallCharacter:
     # definimos la caracteriscitas de la pelota
     def __init__(self):
         # definimos la posicion inicial de la pelota
-        self.initial_x = 0
+        self.initial_x = 100
         self.initial_y = 255
         # definimos la posicion de la pelota que ira variando y la velocidad en la que se movera la pelota
         self.x = self.initial_x
         self.y = self.initial_y
-        self.speed = 1
+        self.speed = float(5)
 
+        #definimos el tiempo que tendra el objeto para hacer sus fisicas
+        self.ball_clock = pygame.time.get_ticks()/1000
+
+        #___Propiedades de la fisica___
+        #gravedad del objeto 
+        self.gravity = float(-9.5)
+        #expresamos las velocidades del movimiento parabolico respecto al eje en coordenadas polares
+        self.v_x = float(10) * np.cos(math.radians(90))
+        self.v_y = float(10) * np.sin(-(math.radians(90)))
+        
     # definimos la funcion que hace los tipos de movimientos
     def moves(self, screen, move_type):
         if move_type['key_d']:
             # para hacer que la pelota se mueva, le sumo 1 a la posicion variante
             pygame.draw.circle(screen, (200, 0, 0), (self.x, self.y), 30)
-            self.x += 1
+            self.x += self.speed
 
         if move_type['key_a']:
             # para hacer que la pelota se mueva, le sumo 1 a la posicion variante
             pygame.draw.circle(screen, (200, 0, 0), (self.x, self.y), 30)
-            self.x += 1
+            self.x -= self.speed
 
         if move_type['static']:
             pygame.draw.circle(screen, (200, 0, 0), (self.x, self.y), 30)
 
-    # esta funcion hara que la pelta vaya para la izquierdad de 1 en uno en el plano cartesiano
+        if move_type['key_space']:
+            self.x = (self.x - (self.v_x * self.ball_clock))
+            self.y = (self.y + self.v_y * self.ball_clock + 1/2 * (self.gravity * (self.ball_clock)**2))
+            
+
